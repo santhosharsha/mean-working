76dce995b79624d1fac6919be0d16364c0bd2a9d
saisanthosh <saisanthosh.saikumar@impigertech.com>
2018-06-25 19:16:58 +0530

    Mean working  with sample db connection and working flow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
-import { AppComponent } from './app.component';
-
+import { BrowserModule } from '@angular/platform-browser';  
+import { NgModule } from '@angular/core';   
+  
+import { HttpModule } from '@angular/http';  
+import { FormsModule } from '@angular/forms';  
+  
+import { AppComponent } from './app.component';  
+  
+import {CommonService} from './common.service';  
+  
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+     BrowserModule,HttpModule,FormsModule,  
   ],
-  providers: [],
+  providers: [CommonService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
